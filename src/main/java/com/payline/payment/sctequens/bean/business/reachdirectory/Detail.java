10820f17c351812bb84bcc05ae22b7c6d56a3cfd
jflorent <jan.florent@mythalesgroup.io>
2020-10-28 11:18:16 +0100

    PAYLAPMEXT-294: Consolidation du filtrage des banques

--- src/main/java/com/payline/payment/sctequens/bean/business/reachdirectory/Detail.java
+++ src/main/java/com/payline/payment/sctequens/bean/business/reachdirectory/Detail.java
@@ -10,11 +10,24 @@ public class Detail {
     @SerializedName("FieldName")
     private String fieldName;
 
+    @SerializedName("Type")
+    private String type;
+
     @SerializedName("Value")
     private String value;
 
     @SerializedName("ProtocolVersion")
     private String protocolVersion;
+
+    public Detail() {
+    }
+
+    public Detail(String api, String fieldName,String type, String value, String protocolVersion) {
+        this.api = api;
+        this.fieldName = fieldName;
+        this.value = value;
+        this.protocolVersion = protocolVersion;
+    }
 
     public String getApi() {
         return api;
